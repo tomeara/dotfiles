bc44a7ecca3023d35102bcca64fe703934b80dad
Ted O'Meara <ted@tedomeara.com>
2014-12-18 21:30:57 -0500

    move all vim dotfiles and plugins to their own repo

--- backup.sh
+++ backup.sh
@@ -1,6 +1,2 @@
-cp -fvr ~/.vim/bundle ./bundle
-cp -fvr ~/.vim/autoload ./autoload
-cp -fvr ~/.vim/colors ./colors
-cp -fvr ~/.vimrc ./.vimrc
 cp -fvr ~/.zprofile ./.zprofile
 cp -fvr ~/.oh-my-zsh/themes/itg-text.zsh-theme ./itg-text.zsh-theme
--- restore.sh
+++ restore.sh
@@ -1,6 +1,3 @@
-cp -Rv ./bundle/ ~/.vim/bundle/
-cp -Rv ./autoload/ ~/.vim/autoload/
-cp -Rv ./colors/ ~/.vim/colors/
-cp -Rv ./.vimrc ~/.vimrc
 cp -Rv ./.zprofile ~/.zprofile
 cp -Rv ./itg-text.zsh-theme ~/.oh-my-zsh/themes/itg-text.zsh-theme
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh
