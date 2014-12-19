96d9bcd1b2772fdccbf49d8d4ce680e5d89e9c03
Ted O'Meara <ted@tedomeara.com>
2014-12-18 21:38:27 -0500

    remove vimrc

--- restore.sh
+++ restore.sh
@@ -1,3 +1,2 @@
-cp -Rv ./.zprofile ~/.zprofile
-cp -Rv ./itg-text.zsh-theme ~/.oh-my-zsh/themes/itg-text.zsh-theme
-git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh
+cp -fRv ./.zprofile ~/.zprofile
+cp -fRv ./itg-text.zsh-theme ~/.oh-my-zsh/themes/itg-text.zsh-theme
