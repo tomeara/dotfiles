66941106f6a6214ad84773f7bc4e56d9c3dd6a2e
Ted O'Meara <ted@tedomeara.com>
2014-12-20 17:14:50 -0500

    added backup/restore for tmux.conf

--- backup.sh
+++ backup.sh
@@ -1,2 +1,3 @@
 cp -fvr ~/.zprofile ./.zprofile
+cp -fvr ~/.tmux.conf
 cp -fvr ~/.oh-my-zsh/themes/itg-text.zsh-theme ./itg-text.zsh-theme
--- restore.sh
+++ restore.sh
@@ -1,2 +1,5 @@
+mv ~/.zprofile ~/.zprofile-old
 cp -fRv ./.zprofile ~/.zprofile
+mv ~/.tmux.conf ~/.tmux.old
+cp -fRv ./.tmux.conf ~/.tmux.conf
 cp -fRv ./itg-text.zsh-theme ~/.oh-my-zsh/themes/itg-text.zsh-theme
